d235f42a2c4533d95ea120d9f9cb3d2bc869d1e0
Scott Foster <thescottfoster@gmail.com>
2018-02-08 17:25:18 -0600

    Building for production

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,14 @@ const apiRoutes = require("./api");
 
 // API Routes
 router.use("/api", apiRoutes);
-
-//temporary login redirect
-
-// router.get("/login", function(req, res) {
-//   res.sendFile(path.join(__dirname, "../views/login.html"));
-// });
     
 // If no API routes are hit, send the React app
 
+router.get('/login', function(req, res) {
+  res.sendFile(path.join(__dirname, "..views/login.html"));
+})
+
 router.use(function(req, res) {
-  console.log('Express loading default page...');
   // res.sendFile(path.join(__dirname, "../client/build/index.html"));
   res.sendFile(path.join(__dirname, "../views/login.html"));
 });
