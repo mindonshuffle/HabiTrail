5c92b9d3967ba5ec04d750573652b80b384281e0
Scott Foster <thescottfoster@gmail.com>
2018-04-02 19:53:11 -0500

    added login / signup toggle

--- client/src/components/LoginDialog/LoginDialog.js
+++ client/src/components/LoginDialog/LoginDialog.js
@@ -18,7 +18,7 @@ import moment from 'moment';
 
 export default class LoginDialog extends React.Component {
   state = {
-    currentDisplay: 'login',
+    signupDisplay: false,
     email: '',
     password: '',
     newEmail: '',
@@ -44,33 +44,74 @@ export default class LoginDialog extends React.Component {
   }
 
   handleSignupClick = () =>{
-    this.setState({currentDisplay: "signup"});
+    this.setState({signupDisplay: true});
   }
 
-  componentWillUpdate = () =>{
-    switch(this.state.currentDisplay){
-      case 'login':
-        break;
-      case 'signup':
-        break;
-    }
+  handleLoginClick = () =>{
+    this.setState({signupDisplay: false});
   }
+
+  // componentWillUpdate = () =>{
+  //   switch(this.state.currentDisplay){
+  //     case 'login':
+  //       break;
+  //     case 'signup':
+  //       break;
+  //   }
+  // }
 
   render() {
+    const signupDisplay = this.state.signupDisplay;
 
-    return (
-      <div>
-        <Card style={{width: '400px'}}>
-          <CardContent style={{backgroundColor: '#5C6BC0'}}>
-            <Grid container spacing="16" alignItems='center' justify="center">
-              <Grid item>
-                <Typography style={{color: '#ffffff'}}type="display1" component="h2">
-                  Habitrail
-                </Typography>
+    const mainContent = signupDisplay ? (
+      <CardContent>
+            <Grid container spacing="16" justify="center">
+              <Grid item xs="8">
+                <TextField
+                  // autoFocus
+                  margin="dense"
+                  id="newEmail"
+                  label="Email Address"
+                  type="text"
+                  value={this.state.newEmail}
+                  onChange={this.handleChange('newEmail')}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs="8">
+                <TextField
+                  // autoFocus
+                  margin="dense"
+                  id="newPassword"
+                  label="Password"
+                  type="password"
+                  value={this.state.newPassword}
+                  onChange={this.handleChange('newPassword')}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs="8">
+                <Grid container justify="flex-end">
+                  <Grid item>
+                    <Button raised color="contrast" style={{backgroundColor: '#5C6BC0'}} onClick={() => this.handleSignupSubmit()}>
+                      Create Account
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs="8">
+                <Grid container justify="center">
+                  <Grid item>
+                   <Button color="primary" onClick={() => this.handleLoginClick()}>
+                    Already have an account? Sign In
+                  </Button> 
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </CardContent>
-          {/* Start Login Card */}
+        ) : 
+        (
           <CardContent>
             <Grid container spacing="16" justify="center">
               <Grid item xs="8">
@@ -109,7 +150,7 @@ export default class LoginDialog extends React.Component {
               <Grid item xs="8">
                 <Grid container justify="center">
                   <Grid item>
-                   <Button color="primary" onClick={() => this.handleSignupClick()}>
+                  <Button color="primary" onClick={() => this.handleSignupClick()}>
                     New User? Sign Up
                   </Button> 
                   </Grid>
@@ -117,101 +158,22 @@ export default class LoginDialog extends React.Component {
               </Grid>
             </Grid>
           </CardContent>
+        );
 
-        {/* Start Signup Card */}
-          <CardContent>
-            <Grid container spacing="16" justify="center">
-              <Grid item xs="8">
-                <TextField
-                  // autoFocus
-                  margin="dense"
-                  id="newEmail"
-                  label="Email Address"
-                  type="text"
-                  value={this.state.newEmail}
-                  onChange={this.handleChange('newEmail')}
-                  fullWidth
-                />
+    return (
+      <div>
+        <Card style={{width: '400px'}}>
+          <CardContent style={{backgroundColor: '#5C6BC0'}}>
+            <Grid container spacing="16" alignItems='center' justify="center">
+              <Grid item>
+                <Typography style={{color: '#ffffff'}}type="display1" component="h2">
+                  Habitrail
+                </Typography>
               </Grid>
-              <Grid item xs="8">
-                <TextField
-                  // autoFocus
-                  margin="dense"
-                  id="newPassword"
-                  label="Password"
-                  type="password"
-                  value={this.state.newPassword}
-                  onChange={this.handleChange('newPassword')}
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs="8">
-                <Grid container justify="flex-end">
-                  <Grid item>
-                    <Button raised color="contrast" style={{backgroundColor: '#5C6BC0'}} onClick={() => this.handleSignupSubmit()}>
-                      Sign In
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Grid item xs="8">
-                <Grid container justify="center">
-                  <Grid item>
-                   <Button color="primary" onClick={() => this.handleLoginClick()}>
-                    Already have an account? Sign In
-                  </Button> 
-                  </Grid>
-                </Grid>
-              </Grid>
-
             </Grid>
           </CardContent>
-        
+          {mainContent}
         </Card>
-
-
-          {/* <p>oopsiedoopsie</p>
-          <DialogTitle>HabiTrail</DialogTitle>
-          <DialogContent>
-              <Typography type="display1" component="h2">
-                Habitrail
-              </Typography>
-
-            <DialogContentText>
-              Please enter your login information
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Description"
-              placeholder="e.g. Take morning medicine"
-              type="text"
-              value={this.state.description}
-              onChange={this.handleChange('description')}
-              fullWidth
-            />
-            <FormControl>
-                <InputLabel htmlFor="goal">
-                    Goal
-                </InputLabel>
-                <Input
-                    id="goal"
-                    value={this.state.goal}
-                    onChange={this.handleChange('goal')}
-                    type="number" 
-                    endAdornment={<InputAdornment position="end">Days</InputAdornment>}
-                />
-            </FormControl>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => saveClick()} color="primary">
-              Add
-            </Button>
-            <Button onClick={() => cancelClick()} color="primary">
-              Cancel
-            </Button>
-          </DialogActions> */}
       </div>
     );
   }
